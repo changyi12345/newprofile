fd6577608a45b4b21b090c85db565300cd5f8a12
choupro108-max <your-email@associated-with-choupro108-max.com>
2026-01-08 18:50:30 +0630

    remove yarn.lock

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  devIndicators: {
-    // @ts-expect-error
-    buildActivity: false,
-    // @ts-expect-error
-    appIsrStatus: false,
-  },
 };
 
 export default nextConfig;
